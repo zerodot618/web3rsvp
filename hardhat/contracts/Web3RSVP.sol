94f02f9314913ecd090df405bbe2df239947123d
huanggz <918031078@qq.com>
2022-11-27 10:35:46 +0800

    defining data structure

--- hardhat/contracts/Web3RSVP.sol
+++ hardhat/contracts/Web3RSVP.sol
@@ -1,3 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+contract Web3RSVP {
+    struct CreateEvent {
+        bytes32 eventId;
+        string eventDataCID;
+        address eventOwner;
+        uint256 eventTimestamp;
+        uint256 deposit;
+        address[] confirmedRSVPs;
+        address[] claimedRSVPs;
+        bool paidOut;
+    }
+
+    mapping(bytes32 => CreateEvent) public idToEvent;
+}
