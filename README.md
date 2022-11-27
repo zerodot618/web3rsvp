c68072a70de058745f58f3817e8cda79e0848729
huanggz <918031078@qq.com>
2022-11-27 17:29:18 +0800

    fetching event details

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - [RainbowKit](https://www.rainbowkit.com/docs/installation#quick-start)
 - [Web3.Storage](https://web3.storage/)
 - [Graph modifiers](https://thegraph.com/docs/en/developer/graphql-api/#all-filters)
+- [Nextjs-getServerSideProps](https://nextjs.org/docs/basic-features/data-fetching/get-server-side-props)
--- next-app/pages/event/[id].js
+++ next-app/pages/event/[id].js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import { gql } from "@apollo/client";
+import client from "../../apollo-client";
 
 import {
   EmojiHappyIcon,
@@ -7,28 +9,33 @@ import {
   LinkIcon
 } from "@heroicons/react/outline";
 
-function Event() {
+
+
+function Event({ event }) {
+  console.log("EVENT:", event)
+
+
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <Head>
-        <title>name | web3rsvp</title>
+        <title>event.name | web3rsvp</title>
         <meta name="description" content={"name"} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="relative py-12">
-        <h6 className="mb-2">time</h6>
+        <h6 className="mb-2">{event.eventTimestamp}</h6>
         <h1 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-5xl mb-6 lg:mb-12">
           name
         </h1>
         <div className="flex flex-wrap-reverse lg:flex-nowrap">
           <div className="w-full pr-0 lg:pr-24 xl:pr-32">
             <div className="mb-8 w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
-              
+
             </div>
             <p>description</p>
           </div>
           <div className="max-w-xs w-full flex flex-col gap-4 mb-6 lg:mb-0">
-           
+
             <div className="flex item-center">
               <UsersIcon className="w-6 mr-2" />
               <span className="truncate">
@@ -49,7 +56,7 @@ function Event() {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  
+
                 </a>
               </span>
             </div>
@@ -62,6 +69,42 @@ function Event() {
 
 export default Event;
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context) {
+  const { id } = context.params;
 
+  const { data } = await client.query({
+    query: gql`
+      query Event($id: String!) {
+        event(id: $id) {
+          id
+          eventId
+          name
+          description
+          link
+          eventOwner
+          eventTimestamp
+          maxCapacity
+          deposit
+          totalRSVPs
+          totalConfirmedAttendees
+          imageURL
+          rsvps {
+            id
+            attendee {
+              id
+            }
+          }
+        }
+      }
+    `,
+    variables: {
+      id: id,
+    }
+  });
+
+  return {
+    props: {
+      event: data.event,
+    },
+  };
 }
