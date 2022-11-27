bd65081976f8604a50759f763a410e7277b20a4f
huanggz <918031078@qq.com>
2022-11-27 12:55:30 +0800

    deploying with alchemy

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" })
+
+const STAGING_INFURA_URL = process.env.STAGING_INFURA_URL
+const STAGING_PRIVATE_KEY = process.env.STAGING_PRIVATE_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: STAGING_INFURA_URL,
+      accounts: [STAGING_PRIVATE_KEY],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    }
+  },
 };
